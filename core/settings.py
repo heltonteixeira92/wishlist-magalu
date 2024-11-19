a01bcaa6015649c348e5122c5075158de3f59ce3
Helton Teixeira de Souza <heltonteixeiradesouza@hotmail.com>
2024-11-18 21:15:32 -0300

    changing spectacular(doc ui) settings to persist authorization and accept image field

--- core/settings.py
+++ core/settings.py
@@ -169,4 +169,8 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': True,
     # OTHER SETTINGS
+    'COMPONENT_SPLIT_REQUEST': True,
+    "SWAGGER_UI_SETTINGS": {
+        "persistAuthorization": True,
+        }
 }
